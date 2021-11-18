70607a52a1f26c073f1a65a2e7acb0de4a8b254b
Oleh Honchar <O.O.Honchar@gmail.com>
2021-11-18 10:39:03 +0200

    feat: add routing to news and user pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import News from './pages/News';
+import User from './pages/User';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/" element={<News />} />
+        <Route path="/users" element={<User />}/>
+      </Routes>
     </div>
   );
 }
